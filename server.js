bd6d58e94f65209fc37704b672223986800fbf0a
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2023-04-20 14:02:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.post("/", (req, res) => {
         stdin: req.body.input,
         language: req.body.lang,
         versionIndex: "0",
-        clientId: "YOUR_CLIENT_ID",
-        clientSecret: "YOUR_CLIENT_SECRET",
+        clientId: "8f209e0f291c17cd8be4dd94a3aa1a4f",
+        clientSecret: "d2cd4c7842379304da55f821fff1f38a4963f088b8dc74aa30455be44a3f0b99",
     };
     request(
         {
