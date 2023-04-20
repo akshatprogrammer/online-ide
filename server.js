62975520b7505b3ba54688d5386161c145702c77
Akshat Jain <67745418+akshatprogrammer@users.noreply.github.com>
2023-04-20 14:17:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/", (req, res) => {
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("app/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "app", "public", "index.html"));
+        res.sendFile(path.resolve(__dirname, "app", "build", "index.html"));
     });
 }
 
